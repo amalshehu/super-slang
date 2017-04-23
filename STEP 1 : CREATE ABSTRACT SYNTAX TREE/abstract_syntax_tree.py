d8a867c0cc2e423bf79b4179eac6fcee35d65b81
amalshehu <amalshehu@gmail.com>
2017-04-24 01:46:15 +0530

    Add arithmetic operations

--- STEP 1 : CREATE ABSTRACT SYNTAX TREE/abstract_syntax_tree.py
+++ STEP 1 : CREATE ABSTRACT SYNTAX TREE/abstract_syntax_tree.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from abc import ABCMeta, abstractmethod
+import operator
 
 class Operator(object):
     "' Declaration of Operators'"
@@ -15,26 +16,28 @@ class Expression(metaclass=ABCMeta):
 class NumericConstant(Expression):
     "'Initialize the numeric property'"
     def __init__(self, value):
-        if not isinstance(value, int, float):
-            raise TypeError("Error01:Invalid type")
+        if not isinstance(value, int):
+            raise TypeError("Slang Error 01:Invalid type")
         self.numeric = value
     def process(self):
         return self.numeric
     def __str__(self):
         return self.numeric
 class BinaryExpression(Expression):
-    def __init__(self, num1, num2, op):
-        super (BinaryExpression, self).__init__(input1, input2, operator))
+    def __init__(self, a, b, op):
+        self.input1 = a
+        self.input2 = b
+        self.operator = op
     def process(self):
         switcher = {
-            self.operator = Operator.PLUS : input1.process() + input2.process(),
-            self.operator = Operator.MINUS : input1.process() + input2.process(),
-            self.operator = Operator.MULT : input1.process() + input2.process(),
-            self.operator = Operator.DIV : input1.process() + input2.process()
+            Operator.PLUS : operator.add(self.input1.process(), self.input2.process()),
+            Operator.MINUS : operator.sub(self.input1.process(), self.input2.process()),
+            Operator.MULT : operator.mul(self.input1.process(), self.input2.process()),
+            Operator.DIV : operator.truediv(self.input1.process(), self.input2.process())
         }
-        return switcher.get(self, "None")
+        return switcher.get(self.operator)
     def __str__(self):
-        return "BinaryExpression(%s %s %s)" % (self.input1, self.input2, self.operator)
+        return "BinaryExp(%s %s %s)" % (self.input1, self.input2, self.operator)
 
 class UnaryExpression(Expression):
     def __init__(self, i, op):
@@ -42,8 +45,12 @@ class UnaryExpression(Expression):
         self.operator = op
     def process(self):
         if self.operator == Operator.PLUS:
-            return self.i.process()
+            return self.input.process()
         elif self.operator == Operator.MINUS:
-            return -self.i.process()
+            return -self.input.process()
         else:
             None
+if __name__ == "__main__":
+    # Abstract Syntax Tree(AST) for 5*4
+    exp = BinaryExpression(NumericConstant(5), NumericConstant(10), Operator.PLUS)
+    print(exp.process())
