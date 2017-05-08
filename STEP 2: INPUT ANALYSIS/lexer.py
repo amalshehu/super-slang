7404aa600a7d6e8861797c66c419ea89600595ff
amalshehu <amalshehu@gmail.com>
2017-05-09 00:20:35 +0530

    Add get_token method

--- STEP 2: INPUT ANALYSIS/lexer.py
+++ STEP 2: INPUT ANALYSIS/lexer.py
@@ -1,4 +1,4 @@
-class Token:
+class Token(object):
     ILLEGAL_TOKEN = -1
     TOKEN_PLUS = 1
     TOKEN_MULT = 2
@@ -9,3 +9,15 @@ class Token:
     TOKEN_DOUBLE = 7
     TOKEN_NULL = 8
 
+class Lexer(object):
+    @classmethod
+    def __init__(self, Expression):
+        self.IExpression = Expression
+        self.length = len(Expression)
+        self.index = 0
+        self.number = None
+    @staticmethod
+    def get_token(self):
+        token = Token.ILLEGAL_TOKEN
+        while self.index < self.length and (self.IExpression[self.index] == '' or self.IExpression[self.index] == '\t'):
+            self.index += 1
